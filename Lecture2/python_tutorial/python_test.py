e033af1ab2821ce908ee5d2c13651ab645e433aa
user.email <tsb03672@naver.com>
2020-01-27 16:56:22 +0900

    cs231n lec1, 2 added + python_test added

---
+++ Lecture2/python_tutorial/python_test.py
@@ -0,0 +1,28 @@
+# python_test.py
+
+# python type
+# numbers
+print('numbers')
+x = 3
+print(type(x))
+print(x)
+print(x ** 2)
+
+# boolean
+print('boolean')
+t = True
+f = False
+print(type(t))
+print(not t)
+print(t and f)
+print(t or f)
+
+# string
+print('string')
+hello = 'hello'
+print(len(hello))
+
+# containers
+print('slicing')
+nums = list(range(5))
+print(nums)
